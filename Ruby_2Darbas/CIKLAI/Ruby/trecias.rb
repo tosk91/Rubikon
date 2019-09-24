3169901cce3f12edf859989fe6a4376430093d98
VidasSalavejus <34349361+VidasSalavejus@users.noreply.github.com>
2019-09-24 16:54:37 +0300

    Update trecias.rb

---
+++ Ruby_2Darbas/CIKLAI/Ruby/trecias.rb
@@ -0,0 +1,19 @@
+e = 0.00001
+n=1;
+n1=n+1;
+a = 0
+a1 = 0
+loop do
+  b = Math.sqrt((n+1)*(n+1)+1) * Math.sqrt((n+1)*(n+1)-1)
+
+  a=(n+1)/b
+  b1 = Math.sqrt((n1+1)*(n1+1)+1) * Math.sqrt((n1+1)*(n1+1)-1)
+  a1=(n1+1)/b1
+  n = n + 1
+  n1 = n1 + 1
+break if !((a1-a).abs>=e)
+end
+
+puts "a[#{n}]: #{a}"
+puts "a[#{n1}]: #{a1}"
+puts "riba yra tarp #{n} ir #{n1} nariu."
