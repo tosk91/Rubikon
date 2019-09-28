be3137ac707558ad465fe77eedf5862a7b7e7292
seph <tomasskam@gmail.com>
2019-09-28 15:55:49 +0300

    delete methods done

--- Ruby_2Darbas/Doubly Linked List/DoublyLinkedList.rb
+++ Ruby_2Darbas/Doubly Linked List/DoublyLinkedList.rb
@@ -4,12 +4,13 @@ class LinkedList
   def initialize(value)
     @head = Node.new(value,nil,nil)
     @tail = @head
-    puts "Please specify how many nodes your list will have: "
+    puts "Please specify how many nodes your list will have:"
     n = gets.chomp.to_i
     node = @head
-    for i in 1..n-1
+    for i in 1..n - 1 do
       #self.add_to_list(i)
-      newNode = Node.new(i,nil,nil)
+      k = value + i
+      newNode = Node.new(k,nil,nil)
       node.next = newNode
       newNode.prev = node
       node = @tail = newNode
@@ -25,11 +26,89 @@ class LinkedList
     node.next = newNode
     newNode.prev = node
     @tail = newNode
+    puts "Added #{value} to the list."
   end
-  
+
+  def del_first(node = @head)
+    if node != nil && node.next != nil
+      node = node.next
+      node.prev = nil
+      @head = node
+      puts "Removed first element from the list."
+    end
+  end
+
+  def del_last(node = @tail)
+    if node != nil && node.prev != nil
+      node = node.prev
+      node.next = nil
+      @tail = node
+      puts "Removed last element from the list."
+    end
+  end
+
+  def del_all(node1 = @head, node2 = @tail)
+    @head = nil
+    @tail = nil
+    puts "The whole list deleted."
+  end
+
+  def del_value(value, node = @head)
+    value_deleted = false
+    if node == nil
+      puts "The list is not created."
+    elsif node == @tail && node.data == value
+      del_all()
+      value_deleted = true
+    else
+      while node != nil
+        if node.data == value
+          node.prev.next = node.next
+          node.next = node.prev
+          puts "Removed #{value} element from the list."
+          value_deleted = true
+          break
+        end
+        node = node.next
+      end
+    end
+    if !value_deleted
+      puts "There is no such element in this list."
+    end
+  end
+
+  def del_pos(pos, node = @head)
+    pos_deleted = false
+    if node == nil
+      puts "The list is not created."
+    elsif node == @tail
+      del_all()
+    elsif pos == 0
+      self.del_first()
+    else
+      for i in 0..pos do
+        if node == nil
+          puts "There is no such position in this list."
+          break
+        elsif i == pos
+          if node.next == nil
+            del_last()
+          else
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            pos_deleted = true
+            puts "Removed element in #{pos} position from the list."
+            break
+          end
+        end
+        node = node.next
+      end
+    end
+  end
+
   def print_forward(node = @head)
     if node == nil
-        puts "The list does not exist. Please create a new one."
+      puts "The list does not exist. Please create a new one."
     else
       puts "The list forward:"
         while node != nil
@@ -41,7 +120,9 @@ class LinkedList
   end
 
   def print_backwards(node = @tail)
-    if node != nil
+    if node == nil
+      puts "The list does not exist. Please create a new one."
+    else
       puts "The list backwards:"
         while node != nil
           print "#{node.data} "
@@ -52,11 +133,23 @@ class LinkedList
   end
 end
 
-theList = LinkedList.new(0)
+theList = LinkedList.new(6)
 theList.print_forward()
 theList.print_backwards()
-n = 17
-theList.add_to_list(n)
-puts "Added #{n} to the end of the list."
+
+v = 17; theList.add_to_list(v)
 theList.print_forward()
-theList.print_backwards()
+
+theList.del_last()
+theList.print_forward()
+
+theList.del_first()
+theList.print_forward()
+
+v = 6; theList.del_value(v)
+theList.print_forward()
+
+v = 0; theList.del_pos(v)
+theList.print_forward()
+# theList.del_all()
+# theList.print_forward()
