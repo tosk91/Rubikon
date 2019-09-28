210127a031f5fc957d4957b42404ddd920be39be
seph <tomasskam@gmail.com>
2019-09-29 00:55:24 +0300

    All methods done, need switch menus

--- Ruby_2Darbas/Doubly Linked List/DoublyLinkedList.rb
+++ Ruby_2Darbas/Doubly Linked List/DoublyLinkedList.rb
@@ -17,18 +17,65 @@ class LinkedList
     end
   end
 
-  def add_to_list(value)
-    node = @head
-    while node.next != nil
-      node = node.next
+  def add_first(value, node = @head)
+    if node != nil
+      if @head == @tail
+        newNode = Node.new(value,nil,nil)
+        newNode.next = node
+        node.prev = newNode
+        @tail = node
+        @head = newNode
+        puts "Added #{value} to the start of the list."
+      else 
+        newNode = Node.new(value,nil,nil)
+        newNode.next = node
+        node.prev = newNode
+        @head = newNode
+        puts "Added #{value} to the start of the list."
+      end
+    else
+      puts "The list is not created."
     end
-    newNode = Node.new(value,nil,nil)
-    node.next = newNode
-    newNode.prev = node
-    @tail = newNode
-    puts "Added #{value} to the list."
   end
 
+  def add_last(value, node = @tail)
+    if node != nil
+      newNode = Node.new(value,nil,nil)
+      newNode.prev = node
+      node.next = newNode
+      @tail = newNode
+      puts "Added #{value} to the end of the list."
+    else 
+      puts "The list is not created."
+    end
+  end
+
+  def add_pos(pos, node = @head)
+    if node == nil
+      puts "The list is not created."
+    else
+      if pos == 0
+        self.add_first(33)
+      else
+        for i in 0..pos do
+          if node == nil
+            puts "There is no such position in this list."
+            break
+          elsif i == pos
+            newNode = Node.new(321, nil, nil)
+            newNode.next = node
+            newNode.prev = node.prev
+            node.prev.next = newNode
+            node.prev = newNode
+            puts "Added new node to the #{pos} position of the list."
+            break
+          end
+          node = node.next
+        end
+      end
+    end
+  end
+  
   def del_first(node = @head)
     if node != nil && node.next != nil
       node = node.next
@@ -135,21 +182,40 @@ end
 
 theList = LinkedList.new(6)
 theList.print_forward()
-theList.print_backwards()
+theList.print_backwards(); puts ""
 
-v = 17; theList.add_to_list(v)
-theList.print_forward()
+# v = 17; theList.add_to_list(v)
+# theList.print_forward();
+# theList.print_backwards(); puts ""
 
-theList.del_last()
-theList.print_forward()
+# theList.del_last()
+# theList.print_forward();
+# theList.print_backwards(); puts ""
 
-theList.del_first()
-theList.print_forward()
+# theList.del_first()
+# theList.print_forward();
+# theList.print_backwards(); puts ""
 
-v = 6; theList.del_value(v)
-theList.print_forward()
+# v = 6; theList.del_value(v)
+# theList.print_forward();
+# theList.print_backwards(); puts ""
 
-v = 0; theList.del_pos(v)
-theList.print_forward()
+# v = 0; theList.del_pos(v)
+# theList.print_forward();
+# theList.print_backwards(); puts ""
+
+# theList.add_first(13)
+# theList.print_forward()
+# theList.print_backwards(); puts ""
+
+# theList.add_last(123)
+# theList.print_forward()
+# theList.print_backwards(); puts ""
+
+# theList.add_pos(0)
+# theList.print_forward()
+# theList.print_backwards(); puts ""
+
 # theList.del_all()
 # theList.print_forward()
+# theList.print_backwards(); puts ""
